ed21a3055525bb6bab972d5e0b6d33045fbfadc6
Tausetii <warrenmax256897@outlook.com>
2025-02-19 18:45:11 -0800

    Added union, difference, and intersection commands to Linked Bags

--- LinkedBag.java
+++ LinkedBag.java
@@ -9,28 +9,43 @@ public class LinkedBag<T> implements BagInterface<T> {
         numberOfEntries = 0;
     }
 
+    @Override
     public int getCurrentSize(){
         return numberOfEntries;
     }
 
+    @Override
     public boolean isEmpty(){
         return numberOfEntries == 0;
     }
 
-    public boolean add(T[] newEntry){
+    @Override
+    public boolean add(T newEntry){
         
-        for(int i = 0; i < newEntry.length; i++){
-            Node newNode = new Node(newEntry[i]);
-            newNode.next = firstNode;
-            firstNode = newNode;
-            numberOfEntries++;
-        }
+        Node newNode = new Node(newEntry);
+        newNode.next = firstNode;
 
+        firstNode = newNode;
+        numberOfEntries++;
 
         return true;
     }
 
-    public boolean remove(T[] anEntry){
+    @Override
+    public T remove(){
+        T result = null;
+
+        if(firstNode != null){
+            result = firstNode.data;
+            firstNode = firstNode.next;
+            numberOfEntries--;
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean remove(T anEntry){
         boolean result = false;
         Node nodeN = getReferenceTo(anEntry);
 
@@ -39,18 +54,19 @@ public class LinkedBag<T> implements BagInterface<T> {
             firstNode = firstNode.next;
 
             numberOfEntries--;
-
             result = true;
         }
 
         return result;
     }
 
+    @Override
     public void clear(){
         firstNode = null;
     }
 
-    public int getFrequencyOf(T[] anEntry){
+    @Override
+    public int getFrequencyOf(T anEntry){
         int frequency = 0;
 
         int counter = 0;
@@ -67,7 +83,8 @@ public class LinkedBag<T> implements BagInterface<T> {
         return frequency;
     }
 
-    public boolean contains(T[] anEntry){
+    @Override
+    public boolean contains(T anEntry){
         boolean found = false;
         Node currentNode = firstNode;
 
@@ -83,6 +100,7 @@ public class LinkedBag<T> implements BagInterface<T> {
         return found;
     }
 
+    @Override
     public T[] toArray(){
         @SuppressWarnings("unchecked")
         T[]result = (T[])new Object[numberOfEntries];
@@ -98,7 +116,7 @@ public class LinkedBag<T> implements BagInterface<T> {
         return result;
     }
 
-    private Node getReferenceTo(T[] anEntry){
+    private Node getReferenceTo(T anEntry){
         boolean found = false;
         Node currentNode = firstNode;
 
@@ -112,6 +130,79 @@ public class LinkedBag<T> implements BagInterface<T> {
         }
 
         return currentNode;
+    }
+
+    @Override
+    public BagInterface<T> union(BagInterface<T> bagToUnify){
+        T[] array1 = this.toArray();
+        T[] array2 = bagToUnify.toArray();
+
+        int resultLength = array1.length + array2.length;
+        BagInterface<T> resultBag = new ResizableArrayBag<>(resultLength);
+
+        for(int i = 0; i < array1.length; i++){
+            resultBag.add(array1[i]);
+        }
+
+        for(int i = 0; i < array2.length; i++){
+            resultBag.add(array2[i]);
+        }
+
+        return resultBag;
+    }
+
+    @Override
+    public BagInterface<T> intersection(BagInterface<T> bagToIntersect){
+        
+        T[] array1 = this.toArray();
+        T[] array2 = bagToIntersect.toArray();
+        int resultLength;
+        int difference = 0;
+
+        if(array1.length > array2.length){
+            resultLength = array1.length;
+        }
+        else{
+            resultLength = array2.length;
+        }
+
+        BagInterface<T> resultBag = new ResizableArrayBag<>(resultLength);
+        BagInterface<T> blacklistBag = new ResizableArrayBag<>(resultLength);
+
+        
+        for(int i = 0; i < resultLength; i++){
+            if(this.contains(array1[i]) && bagToIntersect.contains(array1[i]) && !blacklistBag.contains(array1[i])){
+                difference = Math.abs(this.getFrequencyOf(array1[i]) - bagToIntersect.getFrequencyOf(array1[i]));
+                while(difference != 0){
+                    resultBag.add(array1[i]);
+                    blacklistBag.add(array1[i]);
+                    difference--;
+                }
+            }
+        }
+
+        return resultBag;
+    }
+
+    @Override
+    public BagInterface<T> difference(BagInterface<T> subtractionBag){
+
+        T[] array1 = this.toArray();
+
+        BagInterface<T> resultBag = new ResizableArrayBag<>(this.getCurrentSize());
+        BagInterface<T> blacklistBag = new ResizableArrayBag<>(subtractionBag.getCurrentSize());
+
+        for(int i = 0; i < array1.length; i++){
+            if(!subtractionBag.contains(array1[i]) && !blacklistBag.contains(array1[i])){
+                resultBag.add(array1[i]);
+            }
+
+            if(resultBag.getFrequencyOf(array1[i]) == (this.getFrequencyOf(array1[i]) - subtractionBag.getFrequencyOf(array1[i]))){
+                blacklistBag.add(array1[i]);
+            }
+        }
+
+        return resultBag;
     }
 
     private class Node{
--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -1,30 +1,115 @@
+
+
 public class LinkedBagTest {
     
     public static void main(String[] args) {
         System.out.println("Creating an empty bag");
         BagInterface<String> aBag = new LinkedBag<>();
-        aBag.toArray();
-        aBag.isEmpty();
+        displayBag(aBag);
+        testIsEmpty(aBag,true);
         String[] testStrings1 = {"","B"};
-        aBag.getFrequencyOf(testStrings1);
-        aBag.contains(testStrings1);
-        aBag.remove(testStrings1);
+        testFrequency(aBag, testStrings1);
+        testContains(aBag,testStrings1);
+        testRemove(aBag,testStrings1);
 
         String[] contentsOfBag = {"A","D","B","A","C","A","D"};
-        aBag.add(contentsOfBag);
+        testAdd(aBag,contentsOfBag);
 
-        aBag.isEmpty();
+        testIsEmpty(aBag,false);
         String[] testStrings2 = {"A","B","C","D","Z"};
-        aBag.getFrequencyOf(testStrings2);
-        aBag.contains(testStrings2);
+        testFrequency(aBag,testStrings2);
+        testContains(aBag,testStrings2);
 
         String[] testStrings3 = {"","B","A","C","Z"};
-        aBag.remove(testStrings3);
+        testRemove(aBag,testStrings3);
 
         System.out.println("\nClearing the bag:");
         aBag.clear();
-        aBag.isEmpty();
-        aBag.toArray();
+        testIsEmpty(aBag,true);
+        displayBag(aBag);
 
+    }
+
+    public static void testAdd(BagInterface<String> aBag, String[] content){
+        System.out.print("Adding to the bag: ");
+        for(int index = 0; index < content.length; index++){
+            aBag.add(content[index]);
+            System.out.print(content[index] + " ");
+        }
+        System.out.println();
+        displayBag(aBag);
+    }
+
+    public static void testRemove(BagInterface<String> aBag, String[] tests) {
+        
+        for (int index = 0; index < tests.length; index++){
+         String aString = tests[index];
+         if (aString.equals("") || (aString == null))
+         {
+            // Test remove()
+            System.out.println("\nRemoving a string from the bag:");
+            String removedString = aBag.remove();
+            System.out.println("remove() returns " + removedString);
+         }
+         else
+         {
+            // Test remove(aString)
+            System.out.println("\nRemoving \"" + aString + "\" from the bag:");
+            boolean result = aBag.remove(aString);
+            System.out.println("remove(\"" + aString + "\") returns " + result);
+         } // end if
+         
+         displayBag(aBag);
+    }
+    }
+
+    public static void testIsEmpty(BagInterface<String> aBag, boolean correctResult){
+        
+        System.out.print("\nTesting the method isEmpty with ");
+        if(correctResult){
+            System.out.println("an empty bag:");
+        }else{
+            System.out.println("a bag that is not empty:");
+        }
+
+        System.out.print("isEmpty finds the bag ");
+        if(correctResult && aBag.isEmpty())
+            System.out.println("empty: OK.");
+        else if(correctResult)
+            System.out.println("not empty, but it is empty: ERROR.");
+        else if(!correctResult && aBag.isEmpty())
+            System.out.println("empty, but it is not empty: ERROR.");
+        else
+            System.out.println("not empty: OK.");
+    }
+
+    public static void testFrequency(BagInterface<String> aBag, String[] content){
+        
+        System.out.println("\nTesting the method getFrequencyOf:");
+
+        for (int index = 0; index < content.length; index++){
+        System.out.println("In this bag, the count of " + content[index] + 
+                            " is " + aBag.getFrequencyOf(content[index]));
+        }
+    }   
+
+    public static void testContains(BagInterface<String> aBag, String[] content){
+        
+        System.out.println("\nTesting the method contains:");
+        
+        for (int index = 0; index < content.length; index++){
+        System.out.println("Does this bag contain " + content[index] + 
+                            "? " + aBag.contains(content[index]));
+        }
+    }
+
+    public static void displayBag(BagInterface<String> aBag){
+        System.out.println("The bag contains the following string(s):");
+        Object[] bagArray = aBag.toArray();
+        for(int index = 0; index < bagArray.length; index++){
+            System.out.print(bagArray[index] + " ");
+        }
+
+        System.out.println();
     }
 }
